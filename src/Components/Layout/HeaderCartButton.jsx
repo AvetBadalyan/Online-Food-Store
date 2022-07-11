378f2c7c0781cf91bdba8f011e016da5c75103fd
AvetBadalyan <avetbadalyaN@gmail.com>
2022-07-11 20:33:24 +0400

    feat: functionality to check counts in cart and add non-existing items

--- src/Components/Layout/HeaderCartButton.jsx
+++ src/Components/Layout/HeaderCartButton.jsx
@@ -6,6 +6,7 @@ import CartContext from "./../../Context/cart-context";
 
 export default function HeaderCartButton({ showCartHandler }) {
   const cartCtx = useContext(CartContext);
+
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
