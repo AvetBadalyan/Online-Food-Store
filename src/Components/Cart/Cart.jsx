8996ea01a49af1a4457517c63b609582a5132ef3
AvetBadalyan <avetbadalyaN@gmail.com>
2022-07-11 18:00:20 +0400

    feat: style cart item and restyle modal

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,18 +1,28 @@
 import React, { useContext } from "react";
 import Modal from "../UI/Modal";
 import "./Cart.css";
-import CartContext from './../../Context/cart-context';
+import CartContext from "./../../Context/cart-context";
+import CartItem from "./CartItem";
 
 export default function Cart({ hideCartHandler }) {
-
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className="cart-items">
       {cartCtx.items.map((item) => (
-        <li key={Math.random()}>{item.name} </li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
