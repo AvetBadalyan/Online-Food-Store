0555aa9f2cc87e61f2c99102213dff545ab42cf7
AvetBadalyan <avetbadalyan@gmail.com>
2022-07-27 01:12:29 +0400

    feat: handling fetch error

--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -6,12 +6,18 @@ import MealItem from "./MealItem/MealItem";
 export default function AvailableMeals() {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://online-foodstore-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("fetching error from server, Something went wrong");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -28,13 +34,27 @@ export default function AvailableMeals() {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
-  return <section className="MealsLoading"><p>Loading</p></section>
-}
+    return (
+      <section className="MealsLoading">
+        <p>Loading</p>
+      </section>
+    );
+  }
 
+  if (httpError) {
+    return (
+      <section className="MealsError">
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
