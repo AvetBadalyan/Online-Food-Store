78fb342995b70083eb026e171fae0d9c86bcfc8c
AvetBadalyan <avetbadalyan@gmail.com>
2022-07-26 22:36:43 +0400

    feat: take meals data to firebase and fetch it from server

--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -1,54 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import "./AvailableMeals.css";
 import MealItem from "./MealItem/MealItem";
-import photo1 from "./../../assets/խորոված.jpg";
-import photo2 from "./../../assets/qyabab.jpg";
-import photo3 from "./../../assets/spas.webp";
-import photo4 from "./../../assets/qyufta.jpg";
-import potato from "./../../assets/kartoshka.jpg";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Armenian Khorovats",
-    description: "Pork Loin",
-    price: 22.99,
-    photo: photo1,
-  },
-  {
-    id: "m2",
-    name: "Beef Qebab",
-    description: "Delicious food",
-    price: 16.5,
-    photo: photo2,
-  },
-  {
-    id: "m3",
-    name: "Spas - Armenian Yogurt soup",
-    description: "yogurt, greens, wheat",
-    price: 12.99,
-    photo: photo3,
-  },
-  {
-    id: "m4",
-    name: "Qyufta",
-    description: "Armenian national food",
-    price: 18.99,
-    photo: photo4,
-  },
-
-  {
-    id: "m5",
-    name: "Potato and fat BBQ",
-    description: "250g, served with 1/2 lavash",
-    price: 18.99,
-    photo: potato,
-  },
-];
 
 export default function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://online-foodstore-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+          photo: responseData[key].photo,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
