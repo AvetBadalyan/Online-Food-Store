994b321ef46d67c77cd7cc9fde0fb45e69c5f874
AvetBadalyan <avetbadalyan@gmail.com>
2022-08-04 20:06:07 +0400

    feat: add a fetch with firebase URL to post a sign up request

--- src/Components/Auth/AuthForm.jsx
+++ src/Components/Auth/AuthForm.jsx
@@ -4,29 +4,40 @@ import "./AuthForm.css";
 
 const AuthForm = () => {
   const emailInputRef = useRef();
-    const passwordInputRef = useRef();
-  const [isLogin, setIsLogin] = useState(true);
+  const passwordInputRef = useRef();
+  const [isLoginModeActive, setIsLoginModeActive] = useState(true);
 
   const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
+    setIsLoginModeActive((prevState) => !prevState);
   };
 
-const submitHandler = (e) => {
-  e.preventDefault();
-  const enteredEmail = emailInputRef.current.value;
-  const enteredPassword = passwordInputRef.current.value;
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
 
-  if (isLogin) {
-
-  } else {
-    
-  }
-}
-
+    if (isLoginModeActive) {
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+  };
 
   return (
     <section className="auth">
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <h1>{isLoginModeActive ? "Login" : "Sign Up"}</h1>
       <form>
         <div className="login-control">
           <label htmlFor="email">Your Email</label>
@@ -42,13 +53,15 @@ const submitHandler = (e) => {
           />
         </div>
         <div className="login-actions">
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          <button>{isLoginModeActive ? "Login" : "Create Account"}</button>
           <button
             type="button"
             className="toggle"
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? "Create new account" : "Login with existing account"}
+            {isLoginModeActive
+              ? "Create new account"
+              : "Login with existing account"}
           </button>
         </div>
       </form>
