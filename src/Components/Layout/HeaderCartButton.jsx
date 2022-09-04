8645b1ef6371d53377d8bd6b6d23b1fe4fbc176e
AvetBadalyan <avetbadalyan@gmail.com>
2022-09-04 20:59:37 +0400

    feat: add bump styling with useEffect and setTimeout

--- src/Components/Layout/HeaderCartButton.jsx
+++ src/Components/Layout/HeaderCartButton.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./HeaderCartButton.css";
 import CartIcon from "./../Cart/CartIcon.jsx";
-import { useContext } from "react";
 import CartContext from "./../../Context/cart-context";
 
 export default function HeaderCartButton({ showCartHandler }) {
-  const cartCtx = useContext(CartContext);
+  // add bump effect on Cart button when adding items
+  const [isBump, setIsBump] = useState(false);
+  const buttonWithBump = `button ${isBump ? "bump" : ""}`;
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  // calculate the total number of items in the cart with array reduce method
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setIsBump(true);
+
+    const timer = setTimeout(() => {
+      setIsBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className="button" onClick={showCartHandler}>
+    <button className={buttonWithBump} onClick={showCartHandler}>
       <span className="icon">
         <CartIcon />
       </span>
